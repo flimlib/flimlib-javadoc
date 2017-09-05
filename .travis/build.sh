bafcf7c3b478b10605f7f525e97bdafe451b6939
Curtis Rueden <ctrueden@wisc.edu>
2017-09-05 10:37:56 -0500

    Add Travis CI configuration

---
+++ .travis/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-javadoc.sh
+sh travis-javadoc.sh SLIM-Curve $encrypted_cb3727795fd5_key $encrypted_cb3727795fd5_iv
